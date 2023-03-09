d542aba04861f7444f0412df8327da5f4b120a1b
jesuselian <jesuselian16@hotmail.com>
2023-03-09 10:23:11 -0600

    added statements to determine whether the user wanted to play against a player or bot

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import classGameBoard
 
-newBoard = classGameBoard.GameBoard(8, 8)
-newBoard.print_board()
+# Will ask the user If they would like to play against a player or a bot
+choice = input("Would you like to play against another player or against a bot? ")
+if (choice == "player") or (choice == "Player"):
+    # Will execute functions needed for a 2 player game
+    newBoard = classGameBoard.GameBoard(8, 8)
+    newBoard.print_board()
+
+elif (choice == "bot") or (choice == "Bot"):
+    # Will execute functions needed for a game against the bot
+    newBoard = classGameBoard.GameBoard(8, 8)
+    newBoard.print_board()
+
+else:
+    print("Please choose one of the two options")
