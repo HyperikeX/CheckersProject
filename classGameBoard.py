4e39af962e169a7ada19fa9c43ee85153b33c2fc
HyperikeX <aj.rork@gmail.com>
2023-03-08 20:51:06 -0600

    reworked gameboard and gamespace objects

--- classGameBoard.py
+++ classGameBoard.py
@@ -1,52 +1,55 @@
 class GameSpace:
-    def __init__(self, pos, fill):
-        self.position = pos
-        self.fill = fill
+    def __init__(self, xPos, yPos, content):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.content = content
+        self.sumValue = 0
+        self.isWinning = False
 
-    def check_below(self, width, height):
-        maxsize = height * width
-        if self.position + width > maxsize:
-            return False
-        else:
-            return True
-
-    def check_above(self, width):
-        if self.position - width < 0:
-            return False
-        else:
-            return True
-
-    def check_left(self, width):
-        if self.position % width == 1:
-            return False
-        else:
-            return True
-
-    def check_right(self, width):
-        if self.position % width == 0:
-            return False
-        else:
-            return True
-
-    def print_cords(self, width, height):
-        row = int((self.position / width) + 1)
-        col = int((self.position % width) + 1)
-        newstring = "[" + str(col) + "," + str(row) + "]"
-        return newstring
+    def print_cords(self):
+        newString = "[" + str(self.xPos) + "," + str(self.yPos) + "]"
+        return newString
 
 
 class GameBoard:
+    left = (-1, 0)
+    upLeft = (-1, -1)
+    up = (0, -1)
+    upRight = (1, -1)
+    right = (1, 0)
+    downRight = (1, 1)
+    down = (0, 1)
+    downLeft = (-1, 1)
+    directions = (left, upLeft, up, upRight, right, downRight, down, downLeft)
+    #   Directions:
+    #   [ 0  1  2 ]
+    #   [ 7  -  3 ]
+    #   [ 6  5  4 ]
+
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.maxsize = width * height
-        self.spaces = []
-        for x in range(self.maxsize):
-            self.spaces.append(GameSpace(x, 0))
+        self.board = []
+        for col in range(width):
+            newList = []
+            for row in range(height):
+                newList.append(GameSpace(col, row, 0))
+            self.board.append(newList)
 
     def print_board(self):
-        for space in range(len(self.spaces)):
-            if (space + 1) % self.width == 0:
-                print(self.spaces[space].print_cords(self.width, self.height))
-            else:
-                print(self.spaces[space].print_cords(self.width, self.height), end=" ")
+        for col in range(len(self.board)):
+            for row in range(len(self.board[col])):
+                pos = self.board[row][col].print_cords()
+                print(pos, end=' ')
+            print("")
+
+    def boardCheck(self):
+        for col in range(len(self.board)):
+            for row in range(len(self.board[col])):
+                self.checkNeighbors(self.board[row][col])
+
+    def checkNeighbors(self, position):
+        # check left
+        self.goNext(position, self.directions[0], 1)
+
+    def goNext(self, position, direction, chain):
+        pass
+
