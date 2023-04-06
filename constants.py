a14303609fd6236926d31d3a773b745890b985e3
JElianC <104523035+JElianC@users.noreply.github.com>
2023-04-06 16:36:02 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 700, 600
+ROWS, COLS = 7, 7
+
+# How big a square is in the board
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
